ac8850727dab946618ca7d72d11501c66f97e29d
abdulhafizr <abdulhafizramadan.17@gmail.com>
2020-12-21 20:00:55 +0700

    abstraction crud response

--- route/users.js
+++ route/users.js
@@ -4,43 +4,91 @@ const User = require('../db/user');
 
 router.get("/users", async (request, response) => {
     const users = await User.find({}).then((res) => res);
-    response.json(users);
+    if(users.length > 0) {
+        response.json({
+            status: true,
+            data: users,
+            method : request.method,
+            url: request.url
+        });
+    }else{
+        response.json({
+            status: false,
+            message: "users is empty"
+        })
+    }
 })
 
-router.post('/users', (request, response) => {
+router.post('/users', async (request, response) => {
     const {name, email, password} = request.body;
-    User.create({
-        name: name,
-        email: email,
-        password: password
-    }, (err, res) => {
-        if(err) return console.log(err);
-        response.json(res);
+    const result = await User.create({ name: name, email: email, password: password }).then((res) => res);
+    response.json({
+        status: true,
+        data: result,
+        message: "data created successfully",
+        method: request.method,
+        url: request.url
     })
 })
 
-router.get('/users/:userId', async (request, response) => {
-    const userId = request.params.userId;
-    const result = await User.findOne({_id: userId}).then((res) => res);
-    response.json(result);
+router.get('/users/:name', async (request, response) => {
+    const name = request.params.name;
+    const results = await User.find({name: name}).then((res) => res);
+    if(results.length > 0) {
+        response.json({
+            status: true,
+            data: results,
+            method : request.method,
+            url: request.url
+        });
+    }else{
+        response.json({
+            status: false,
+            message: "data not found",
+            method: request.method,
+            url: request.url
+        });
+    }
 })
 
-router.put('/users/:id', (request, response) => {
+router.put('/users/:id', async (request, response) => {
     const {id, name, email, password} = request.body;
-    User.updateOne({id}, {
-        name,
-        email,
-        password
-    }, (err, res) => {
-        if(err) return console.log(err);
-        response.json(res);
-    })
+    const result = await User.updateOne({id}, { name, email, password }).then((res) => res);
+    if(result.ok) {
+        response.json({
+            status: true,
+            message: "data has updated",
+            method: request.method,
+            url: request.url
+        });
+    }else{
+        response.json({
+            status: false,
+            message: "data not updated",
+            method: request.method,
+            url: request.url
+        });
+    }
 })
 
 router.delete('/users/:userId', async (request, response) => {
     const _id = request.params.userId;
     const result = await User.deleteOne({_id}).then(res => res);
-    response.json(result)
+    if(result.ok) {
+        response.json({
+            status: true,
+            message: "data has deleted",
+            method: request.method,
+            url: request.url
+        });
+    }else{
+        response.json({
+            status: false,
+            message: "data not deleted",
+            method: request.method,
+            url: request.url
+        });
+    }
 })
 
 module.exports = router;
